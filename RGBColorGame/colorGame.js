434449ed54b8af8bda9e4ef934ab511b81cd851c
JamesRW1971 <JamesRW1971@yahoo.com>
2020-01-02 23:19:00 -0500

    pause web bootcamp

---
+++ RGBColorGame/colorGame.js
@@ -0,0 +1,36 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++)
+{
+    // add initial colors
+    squares[i].style.backgroundColor = colors[i];
+
+    // add listeners
+    squares[i].addEventListener("click", function(){
+        // grab color of picked square
+        var clickedColor = this.style.backgroundColor;
+        
+        // compare to picked color
+        if (clickedColor === pickedColor)
+        {
+            alert("correct");
+        }
+        else
+        {
+            alert("wrong");
+        }
+
+    });
+}
